466801b992c74f869473d3f2ebe0238a3a52c2d7
Soupin1205 <Soupin1205@gmail.com>
2026-02-14 23:15:02 +0700

    week5

--- Start code/exercise-5/src/App.jsx
+++ Start code/exercise-5/src/App.jsx
@@ -5,21 +5,6 @@ import "./index.css";
 function App() {
   return (
     <div className="container">
-<<<<<<< HEAD
-      <h1>My Card List</h1>
-
-      <div className="card-wrapper">
-        {data.map((item) => (
-          <Card
-            key={item.id}
-            image={item.image}
-            name={item.name}
-            hobby={item.hobby}
-            address={item.address}
-          />
-        ))}
-      </div>
-=======
       {data.map((item) => (
         <Card
           key={item.id}
@@ -29,7 +14,6 @@ function App() {
           hobby={item.hobby}
         />
       ))}
->>>>>>> 9f184f8 (week5)
     </div>
   );
 }
--- Start code/exercise-5/src/data.js
+++ Start code/exercise-5/src/data.js
@@ -6,54 +6,30 @@ import img4 from "./assets/4.png";
 const data = [
   {
     id: 1,
-<<<<<<< HEAD
-    name: "Sok Dara",
-    hobby: "Football",
-    address: "Phnom Penh",
-=======
     name: "Ding",
     className: "Class C",
     hobby: "Soccer, Tennis",
->>>>>>> 9f184f8 (week5)
     image: img1
   },
-  {
+  {     
     id: 2,
-<<<<<<< HEAD
-    name: "Lina Chan",
-    hobby: "Reading",
-    address: "Siem Reap",
-=======
     name: "Dong",
     className: "Class A",
     hobby: "Piano",
->>>>>>> 9f184f8 (week5)
     image: img2
   },
   {
     id: 3,
-<<<<<<< HEAD
-    name: "Vuthy Kim",
-    hobby: "Gaming",
-    address: "Battambang",
-=======
     name: "Dang",
     className: "Class C",
     hobby: "Video Games",
->>>>>>> 9f184f8 (week5)
     image: img3
   },
   {
     id: 4,
-<<<<<<< HEAD
-    name: "Maly Nguon",
-    hobby: "Traveling",
-    address: "Kampot",
-=======
     name: "Dung",
     className: "Class B",
     hobby: "I like all",
->>>>>>> 9f184f8 (week5)
     image: img4
   }
 ];
